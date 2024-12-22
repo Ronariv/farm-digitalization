6440609cb209d52de718f130e9da2b7cd0c94893
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-12-22 17:57:07 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,34 @@
 import Image from "next/image";
-export default function Home() {
+
+import { Button } from "@/components/ui/button"
+import { Inter } from "next/font/google";
+import Link from "next/link";
+
+export default function OnBoarding() {
   return (
-    <h1>halo guys</h1>
+    <div className="flex h-screen">
+
+      <div className="flex-1 bg-gray-200"></div>
+
+      <div className="flex justify-end items-center flex-2 p-4 pr-20 mr-20 pl-20 ml-20">
+        <div className="flex flex-col space-y-7 ">
+
+          <Link href="/loginPage">
+          <Button
+           className="w-[15.5625rem] h-[3.25rem] rounded-[10px] bg-[#248543] text-white hover:text-[#248543]"
+          >
+            Login
+          </Button>
+          </Link>
+
+          <Button
+            className="w-[15.5625rem] h-[3.25rem] rounded-[10px] border border-[#248543] text-[#248543] hover:bg-[#248543]/10"
+          >
+            Register
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
+
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,8 +11,14 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-primary text-primary bg-transparent hover:bg-primary/10",
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2",
