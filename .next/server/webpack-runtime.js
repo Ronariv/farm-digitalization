2265c4d272e3047a0e46c08faf54852ed57a0b21
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-12-26 23:08:55 +0800

    edited register 1&2 page
    edited register 1&2 page

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e6035616d7224338")
+/******/ 		__webpack_require__.h = () => ("2d8c1bd95a489c65")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("52f4d4fd550ae112")
+/******/ 		__webpack_require__.h = () => ("101104c179649177")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
