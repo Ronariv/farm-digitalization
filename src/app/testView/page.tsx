60a6a2a3d04492ed065557dee14235d9e226424e
HalizaSyafa27 <oktavianihaliza@gmail.com>
2025-01-01 22:25:13 +0700

    feat: adding genderIcon component
    adding genderIcon component

--- src/app/testView/page.tsx
+++ src/app/testView/page.tsx
@@ -3,16 +3,19 @@
 import React, { useState } from 'react';
 // import PhaseLabelButton from "@/components/ui/PhaseLabel/PhaseLabelButton";
 // import { phaseLabels } from "@/data/phaseLabels";
-import HealthStatus from "@/components/ui/healthStatus"; // Import komponen
+import HealthStatus from "@/components/ui/healthStatus"; 
+import GenderIcon from '@/components/ui/genderIcon';
 
 const App: React.FC = () => {
 //   const [phases, setPhases] = useState(phaseLabels);
 //   const kondisi: StatusType = "sehat";
 
   return (
-    <div className="icon-health">
-      <HealthStatus status="tidakSehat" />
+    <div className="icon-gender">
+      {/* <HealthStatus status="tidakSehat" /> */}
+      <GenderIcon gender="betina" />
     </div>
+    
   );
 };
 
