a68641e97e3a932043cdbefeb957ee5112656fe8
HalizaSyafa27 <oktavianihaliza@gmail.com>
2025-01-07 16:23:26 +0700

    add icon sapi
    add icon sapi

--- src/app/OwnerViewPage/detailStatisticsPage/page.tsx
+++ src/app/OwnerViewPage/detailStatisticsPage/page.tsx
@@ -1,83 +1,127 @@
-import React from 'react';
-import DetailAnimalFemaleCard from '@/components/ui/DetailAnimalFemaleCard/detailAnimalFemaleCard';
-import animalFemaleCategories from '@/models/detailAnimalFemaleCategories';
-import DetailAnimalMaleCard from '@/components/ui/DetailAnimalMaleCard/detailAnimalMaleCard';
-import animalMaleCategories from '@/models/detailAnimalMaleCategories';
-import animalDiagnosedCategories from '@/models/detailAnimalDiagnosedCategories';
-import StatisticMilk from '@/components/ui/StatisticsMilk/statisticsMilk';
-import { statisticsMilkData } from "@/models/statisticsMilkModel";
-import StatisticsLactation from '@/components/ui/StatisticsLactation/statisticsLactation';
+'use client'
 
-const AnimalCategoriesPage = () => {
-    const filteredAnimalFemaleCategories = animalFemaleCategories.filter(
-        (category) => category.type === 'kambing' 
+import React, { useState } from 'react';
+// import PhaseLabelButton from "@/components/ui/PhaseLabel/PhaseLabelButton";
+// import { phaseLabels } from "@/data/phaseLabels";
+// import HealthStatus from "@/components/ui/healthStatus"; 
+// import GenderIcon from '@/components/ui/genderIcon';
+import EditButton from '@/components/ui/EditButton/editButton';
+import AddButton from '@/components/ui/AddButton/addButton';
+import InviteOperatorButton from '@/components/ui/InviteOperatorButton/inviteOperatorButton';
+import UpdateButton from '@/components/ui/UpdateButton/updateButton';
+import PrintButton from '@/components/ui/PrintButton/printButton';
+import FilterButton from '@/components/ui/Filter/filterButton';
+import SortByButton from '@/components/ui/SortBy/sortBy';
+import YearAndMonthPicker from '@/components/ui/YearAndMonthPicker/yearAndMonthPicker';
+import Sidebar from '@/components/ui/Sidebar/sidebar';
+import SearchBar from '@/components/ui/SearchBar/searchBar';
+import CategoryAnimalCard from '@/components/ui/CategoryAnimalCard/categoryAnimalCard';
+import animalCategories from '@/models/animalCategories';
+import OperatorProfile from '@/components/ui/OperatorProfile/operatorProfile';
+import Image from 'next/image';
+import OwnerProfile from '@/components/ui/OwnerProfile/ownerProfile';
+import Breadcrumbs from '@/components/ui/Breadcrumbs/breadcrumbs';
+import { usePathname } from 'next/navigation';
+
+const App: React.FC = () => {
+
+  const [breadcrumb, setBreadcrumb] = useState('Statistik');
+
+    const handleUpdate = () => {
+        alert("Tombol Invite Ternak diklik!");
+    };
+
+    const handlePrint = () => {
+        alert("Tombol print sudah diklik!");
+    };
+
+    const handleFilter = () => {
+      alert("Tombol filter di klik");
+    };
+
+    const handleSortBy = () => {
+      alert("Tombol sort di klik");
+    };
+
+    function getMenuLabel(pathname: string): string {
+      switch (pathname) {
+        case '/OwnerViewPage':
+          return 'Statistik';
+        case '/livestockOwnerPage':
+          return 'Ternak Anda';
+        case '/activityOwnerPage':
+          return 'Aktivitas';
+        case '/settingsOwnerPage':
+          return 'Pengaturan';
+        default:
+          return 'Halaman Tidak Ditemukan';
+      }
+    }
+
+    return (
+    <div className="layout">
+      <div className="sidebar">
+      <Sidebar setBreadcrumb={setBreadcrumb} />
+      </div>
+
+      <div className="main-content">
+        
+        <div className="header">
+          
+          <div className="searchbar">
+          <SearchBar />
+          </div>
+
+          <div className="operatorProfile">
+          <OperatorProfile
+          src="/operator.jpeg" 
+          altText=" "
+          />
+          </div>
+
+          <div>
+            <OwnerProfile
+            src="/OwnerProfile.jpeg"
+            altText=" "
+            />
+          </div>
+
+        </div>
+       
+        <div className="content">
+        <div className="menuSection">
+          <div>
+          <Breadcrumbs
+            breadcrumbs={[
+              { label: 'Peternakan Widjaya', href: '/OwnerViewPage' },
+              { label: 'Statistik', href: '/OwnerViewPage' },
+            ]}
+          />
+          </div>
+          <div className="menuHeader">
+            <h1 className="menuTittle">Statistik</h1>
+            <div className="yearAndMonthPicker">
+              <YearAndMonthPicker/>
+            </div>
+          </div>
+          <div className="animalCategoriesCard">
+            {animalCategories.map((category) => (
+              <CategoryAnimalCard
+                key={category.type}
+                icon={category.icon}
+                title={category.title}
+                total={category.total}
+                maleCount={category.maleCount}
+                femaleCount={category.femaleCount}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </div>
     );
-
-    const filteredAnimalMaleCategories = animalMaleCategories.filter(
-        (category) => category.type === 'kambing'
-    )
-
-    const filteredAnimalDiagnosedCategories = animalDiagnosedCategories.filter(
-      (category) => category.type === 'kambing'
-    )
-
-    
-      
-  return (
-    <div>
-      <div style={{ display: 'flex', gap: '16px', flexWrap: 'wrap', justifyContent: 'center'}}>
-      {filteredAnimalFemaleCategories.map((category) => (
-        <DetailAnimalFemaleCard
-          key={category.type} 
-          title={category.tittle}
-          total={category.total}
-          pedet={category.pedet}
-          dara={category.dara}
-          siapKawin={category.siapKawin}
-          hamil={category.hamil}
-          menyusui={category.menyusui}
-        />
-      ))}
-
-      {filteredAnimalMaleCategories.map((category) => (
-        <DetailAnimalMaleCard
-          key={category.type} 
-          title={category.tittle}
-          total={category.total}
-          pedet={category.pedet}
-          siapKawin={category.siapKawin}
-        />
-      ))}
-
-      {filteredAnimalDiagnosedCategories.map((category) => (
-        <DetailAnimalMaleCard
-          key={category.type} 
-          title={category.tittle}
-          total={category.total}
-          pedet={category.sakit}
-          siapKawin={category.hilang}
-        />
-      ))}
-
-    </div>
-
-    <div className="statistikMilk">
-        {/* <StatisticMilk
-        average={2000}
-        monthsData={monthsData}
-        minMonth="Agustus"
-        maxMonth="Desember"
-      /> */}
-    {/* <StatisticMilk filterBy="year" filterValue={2019} /> */}
-
-    <StatisticsLactation filterBy="year" filterValue={2019}/>
-    </div>
-
-    </div>
-
-
-    
-  );
 };
 
-export default AnimalCategoriesPage;
+export default App;
