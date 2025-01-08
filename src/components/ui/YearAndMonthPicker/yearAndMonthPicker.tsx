477d69ff1717ed0f21784a08b4011075d4c17145
HalizaSyafa27 <oktavianihaliza@gmail.com>
2025-01-08 16:42:36 +0700

    feat: edit dropdown color at YearAndMonthPicker component
    feat: edit dropdown color at YearAndMonthPicker component

--- src/components/ui/YearAndMonthPicker/yearAndMonthPicker.tsx
+++ src/components/ui/YearAndMonthPicker/yearAndMonthPicker.tsx
@@ -168,7 +168,7 @@ const YearAndMonthPicker: React.FC = () => {
         >
           <path
             d="M4.5 6L8.5 10L12.5 6"
-            stroke="#000"
+            stroke="#FFFFFF"
             strokeWidth="1.5"
             strokeLinecap="round"
             strokeLinejoin="round"
@@ -197,7 +197,7 @@ const YearAndMonthPicker: React.FC = () => {
                   <g clipPath="url(#clip0_1162_11826)">
                     <path
                       d="M6.42728 16C6.81818 16 7.12727 15.8278 7.34546 15.4924L15.9363 2.00338C16.1 1.74049 16.1637 1.54106 16.1637 1.33256C16.1637 0.833976 15.8364 0.507629 15.3364 0.507629C14.9728 0.507629 14.7727 0.625476 14.5546 0.969951L6.3909 13.9422L2.15454 8.41246C1.92727 8.09517 1.7 7.96826 1.37273 7.96826C0.854545 7.96826 0.5 8.3218 0.5 8.82039C0.5 9.02889 0.590909 9.26459 0.763636 9.48211L5.48182 15.4742C5.75454 15.8278 6.03637 16 6.42728 16Z"
-                      fill="#248543"
+                      fill="#FFFFFF"
                     />
                   </g>
                   <defs>
@@ -205,7 +205,7 @@ const YearAndMonthPicker: React.FC = () => {
                       <rect
                         width="16"
                         height="16"
-                        fill="white"
+                        fill="FFFFFF"
                         transform="translate(0.5)"
                       />
                     </clipPath>
